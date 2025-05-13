17e704b69e94746ddaf4120bf3780be2c2c051db
Satyajit Samantaray <52175257+chinabudhi123@users.noreply.github.com>
2025-05-13 20:41:54 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,7 +6,7 @@ sudo dnf update -y
 sudo dnf install -y httpd
 
 echo "🌐 Copying webpage files to Apache root..."
-sudo cp -r /root/apache-webpage-deploy/webpage/* /var/www/html/
+sudo cp -r /home/ec2-user/app/webpage/* /var/www/html/
 
 echo "🚀 Starting Apache web server..."
 sudo systemctl enable httpd
